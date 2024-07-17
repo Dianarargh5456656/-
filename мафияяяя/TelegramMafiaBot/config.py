0a0ab80f06c4ef35a6b2ac07db638347dd8f300c
Dianarargh5456656 <kalina.daian@yandex.ru>
2024-07-17 10:26:02 +0300

    Add files via upload

---
+++ мафияяяя/TelegramMafiaBot/config.py
@@ -0,0 +1,14 @@
+from aiogram.types import *
+
+TOKEN_TG = "7420871593:AAGmjGcTcN7f9w2BdwNLldkLdacCbtuFCb4"
+
+ADMINS = [583149224, 944220894, 808663657, 1927158967]
+#583149224, 944220894, 808663657, 1927158967
+
+
+class Text:
+
+    HELP = None
+
+    class Commands: 
+help  = ["/start", "/help"]
